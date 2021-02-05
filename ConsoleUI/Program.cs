8e104d35c53a3cc3c93aef7e610caffbfa9b0e30
esrasnck <esrasancak@hotmail.com>
2021-02-05 23:09:03 +0300

    Basic olarak program.cs'deki main metodu hazırlandı.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Araba yılı giriniz");
+            string modelYear = Console.ReadLine();
+            Console.WriteLine("araba ücreti giriniz");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Araba açıklaması giriniz");
+            string aciklama = Console.ReadLine();
+
+       
+
+
             CarManager carManager = new CarManager(new EfCarDal());
+
+            Car car = new Car();
+            car.ModelYear = modelYear;
+            car.DailyPrice = price;
+            car.Description = aciklama;
+            car.BrandId = 1;  // default olarak. şimdilik
+            car.ColorId = 1; // default olarak. şimdilik
+            carManager.Add(car);
+
+
 
             foreach (Car item in carManager.GetAll())
             {
